1065870ce5b02b4e3841485a7fe749dcc7f79060
Thomas Zierer <thomas.zierer@muenchen-mail.de>
2024-06-05 10:42:04 +0200

    Merge branch 'develop' into feature/selectiondialog

--- de.tgmz.zdev/de.tgmz.zdev.view/src/de/tgmz/zdev/view/HFSSelectionDialog.java
+++ de.tgmz.zdev/de.tgmz.zdev.view/src/de/tgmz/zdev/view/HFSSelectionDialog.java
@@ -12,42 +12,21 @@ package de.tgmz.zdev.view;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
-<<<<<<< Upstream, based on develop
-=======
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
->>>>>>> e96cb40 Add transfer mode
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.ElementTreeSelectionDialog;
 
 import com.ibm.cics.zos.comm.IZOSConstants;
 import com.ibm.cics.zos.comm.IZOSConstants.FileType;
 import com.ibm.cics.zos.model.HFSFolder;
-<<<<<<< Upstream, based on develop
-import com.ibm.cics.zos.ui.HFSSelectionTree;
-=======
 import com.ibm.cics.zos.model.IZOSConnectable;
 import com.ibm.cics.zos.ui.HFSLabelProvider;
->>>>>>> e96cb40 Add transfer mode
 
-<<<<<<< Upstream, based on develop
-import de.tgmz.zdev.connection.ZdevConnectable;
-import de.tgmz.zdev.preferences.ZdevPreferenceConstants;
-
-=======
->>>>>>> e96cb40 Add transfer mode
 /**
  * Class for selecting a {@link HFSFolder}.
  */
 
-<<<<<<< Upstream, based on develop
-public class HFSSelectionDialog extends AbstractSelectionDialog {
-    /** HFS Selection Tree */
-    private HFSSelectionTree hst;
-    private IZOSConstants.FileType transferMode = FileType.EBCDIC;
-    
-    public HFSSelectionDialog(final Shell parentShell) {
-    	super(parentShell);
-=======
 public class HFSSelectionDialog extends ElementTreeSelectionDialog {
     private IZOSConstants.FileType transferMode = FileType.EBCDIC;
     
@@ -55,43 +34,8 @@ public class HFSSelectionDialog extends ElementTreeSelectionDialog {
     	super(parentShell, new HFSLabelProvider(), new HfsTreeContentProvider(connectable));
     	
     	setInput(new HFSFolder(connectable, root));
->>>>>>> e96cb40 Add transfer mode
     }
 
-<<<<<<< Upstream, based on develop
-    @Override
-    protected void insertControls(GridData gridData) {
-        // PDS only, single selection only
-        hst = new HFSSelectionTree(shell
-        		, ZdevConnectable.getConnectable()
-        		, de.tgmz.zdev.preferences.Activator.getDefault().getPreferenceStore().getString(ZdevPreferenceConstants.USS_HOME)
-        		, false);
-        hst.addTreeSelectionListener(this);
-        
-		TransferModeCompositeFactory.getInstance().createComposite(shell
-				, gridData 
-				, createSelectionAdapter(FileType.EBCDIC)
-				, createSelectionAdapter(FileType.BINARY)
-				, createSelectionAdapter(FileType.ASCII));
-	}
-    
-    public HFSFolder getTarget() {
-    	return (HFSFolder) hst.getSelection();
-    }
-
-	@Override
-	public void widgetSelected(SelectionEvent event) {
-		this.btnOk.setEnabled(hst.getSelection() != null);
-	}
-
-	public HFSSelectionTree getHst() {
-		return hst;
-	}
-
-	public IZOSConstants.FileType getTransferMode() {
-		return transferMode;
-	}
-=======
 	@Override
 	protected Control createButtonBar(Composite parent) {
 		TransferModeCompositeFactory.getInstance().createComposite(parent
@@ -107,7 +51,6 @@ public class HFSSelectionDialog extends ElementTreeSelectionDialog {
 		return transferMode;
 	}
 	
->>>>>>> e96cb40 Add transfer mode
 	private SelectionAdapter createSelectionAdapter(FileType target) {
 		return new SelectionAdapter() {
 			@Override
---
+++ de.tgmz.zdev/de.tgmz.zdev.view/src/de/tgmz/zdev/view/HfsTreeContentProvider.java
@@ -0,0 +1,73 @@
+/*********************************************************************
+* Copyright (c) 30.05.2024 Thomas Zierer
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+package de.tgmz.zdev.view;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ibm.cics.core.comm.ConnectionException;
+import com.ibm.cics.zos.model.HFSEntry;
+import com.ibm.cics.zos.model.HFSFolder;
+import com.ibm.cics.zos.model.IZOSConnectable;
+import com.ibm.cics.zos.model.PermissionDeniedException;
+
+public class HfsTreeContentProvider implements ITreeContentProvider {
+	private static final Logger LOG = LoggerFactory.getLogger(HfsTreeContentProvider.class);
+	private static final HFSEntry[] EMPTY = new HFSEntry[0];
+	
+	private IZOSConnectable connectable;
+
+	public HfsTreeContentProvider(IZOSConnectable connectable) {
+		super();
+		this.connectable = connectable;
+	}
+
+	@Override
+	public Object[] getElements(Object inputElement) {
+		LOG.debug("getElements {}", inputElement);
+		
+		if (inputElement instanceof HFSFolder f) {
+			try {
+				List<HFSEntry> children = connectable.getChildren(f, true);
+			
+				return  children.toArray(new HFSEntry[children.size()]);
+			} catch (FileNotFoundException | PermissionDeniedException | ConnectionException e) {
+				LOG.error("Cannot get children of {}", inputElement);
+			}
+		}
+		
+		return EMPTY;
+	}
+
+	@Override
+	public Object[] getChildren(Object parentElement) {
+		LOG.debug("getChildren {}", parentElement);
+		
+		return getElements(parentElement);
+	}
+
+	@Override
+	public Object getParent(Object element) {
+		LOG.debug("getParent {}", element);
+		
+		return ((HFSEntry) element).getParent();
+	}
+
+	@Override
+	public boolean hasChildren(Object element) {
+		LOG.debug("hasChildren {}", element);
+		
+		return element instanceof HFSFolder && getChildren(element).length > 0;
+	}
+}
