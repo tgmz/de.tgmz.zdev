0e25f9edfef15121b13c7a6f081d5932ccdc13d4
Thomas Zierer <thomas.zierer@muenchen-mail.de>
2026-07-01 11:38:21 +0200

    Hotfix: Separate ZOWE and z/Dev

---
+++ de.tgmz.zdev/de.tgmz.zdev.product/zdev.sh
@@ -0,0 +1,9 @@
+# Copyright (c) 01.07.2026 Thomas Zierer
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+java -Declipse.pluginCustomization=config.ini -Xmx1024m -jar plugins/org.eclipse.equinox.launcher_1.6.700.v20240213-1244.jar -consoleLog -showSplash
